4462f4fe34cf86c1b649811b78376d71ed96b84b
Brian McKenna <brian@brianmckenna.org>
2018-06-20 20:19:57 +1000

    Add hie80 back in
    Using the old version, but it exists.

--- default.nix
+++ default.nix
@@ -1,6 +1,13 @@
 { pkgs ? (import (import ./fetch-nixpkgs.nix) {}) }:
 
-with pkgs; rec {
+let
+  hie80Pkgs = (import ./ghc-8.0.nix { inherit pkgs; }).override {
+    overrides = self: super: {
+      # TODO: unnecessary with https://github.com/input-output-hk/stack2nix/issues/84#issuecomment-362035573
+      Cabal = null;
+    };
+  };
+in with pkgs; rec {
  stack2nix = import (pkgs.fetchFromGitHub {
    owner = "sectore";
    repo = "stack2nix";
@@ -9,9 +16,11 @@ with pkgs; rec {
  }) { inherit pkgs; };
  hies = runCommandNoCC "hies" {} ''
    mkdir -p $out/bin
+   ln -s ${hie80}/bin/hie $out/bin/hie-8.0
    ln -s ${hie82}/bin/hie $out/bin/hie-8.2
    ln -s ${hie84}/bin/hie $out/bin/hie-8.4
  '';
- hie84 = (import ./ghc-8.4.nix { inherit pkgs; }).haskell-ide-engine;
+ hie80 = hie80Pkgs.haskell-ide-engine;
  hie82 = (import ./ghc-8.2.nix { inherit pkgs; }).haskell-ide-engine;
+ hie84 = (import ./ghc-8.4.nix { inherit pkgs; }).haskell-ide-engine;
 }
