d9fc03cf15e44d6b86c3495b6009d20db17aa7f1
Silvan Mosberger <infinisil@icloud.com>
2018-08-24 19:56:11 +0200

    Include hie-wrapper in hies

--- default.nix
+++ default.nix
@@ -28,11 +28,15 @@ in with pkgs; rec {
    rev = "b8668e17d5b3c5035bd88720c637ae1d333c2ebe";
    sha256 = "0dv6xy89qyrhw3yxl8qvh72skgglpvx9h5ynmhb1j8nrnxv2y5vs";
  }) { inherit pkgs; };
- hies = runCommandNoCC "hies" {} ''
+ hies = runCommandNoCC "hies" {
+   buildInputs = [ makeWrapper ];
+ } ''
    mkdir -p $out/bin
    ln -s ${hie80}/bin/hie $out/bin/hie-8.0
    ln -s ${hie82}/bin/hie $out/bin/hie-8.2
    ln -s ${hie84}/bin/hie $out/bin/hie-8.4
+   makeWrapper ${hie84}/bin/hie-wrapper $out/bin/hie-wrapper \
+     --prefix PATH : $out/bin
  '';
  hie80 = jse hie80Pkgs.haskell-ide-engine;
  hie82 = jse (import ./ghc-8.2.nix { inherit pkgs; }).haskell-ide-engine;
