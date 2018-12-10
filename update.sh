a7ef4c4ceef1dbf46aabff68a4b9bd86d41d0886
Domen Kožar <domen@dev.si>
2018-12-10 13:21:55 +0000

    update.sh: remove jq dance

--- update.sh
+++ update.sh
@@ -3,15 +3,13 @@
 set -xe
 
 # needed since that's how stack2nix finds compiler, etc
-export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-18.09.tar.gz
-NIXPKGS_COMMIT=$(nix-shell -p jq --run "jq -r '.rev' nixpkgs-src.json")
+NIXPKGS_COMMIT=$(nix-instantiate --eval -E "(builtins.fromJSON (builtins.readFile ./nixpkgs-src.json)).rev")
 export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$NIXPKGS_COMMIT.tar.gz
 
-STACK2NIX=$(nix-build -A stack2nix)/bin/stack2nix
+STACK2NIX=$(nix-build -A stack2nix --no-out-link)/bin/stack2nix
 
 COMMIT=1a5640f4790bde364dcd0a61617c5ca5b85b145a
 URL=https://github.com/haskell/haskell-ide-engine.git
-
 
 $STACK2NIX --revision $COMMIT $URL --stack-yaml=stack-8.6.2.yaml > ghc-8.6.nix
 $STACK2NIX --revision $COMMIT $URL --stack-yaml=stack-8.4.4.yaml > ghc-8.4.nix
