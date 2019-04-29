5cb0d2411cf9b75b46d2d5b5f2c1cbcfa8d11102
R D'Netto <rdnetto@gmail.com>
2019-04-29 07:04:03 -0400

    Manage hie with niv

--- update.sh
+++ update.sh
@@ -3,14 +3,13 @@
 set -xe
 
 # needed since that's how stack2nix finds compiler, etc
-NIXPKGS_COMMIT=$(nix eval --raw "(builtins.fromJSON (builtins.readFile ./nixpkgs-src.json)).rev")
-export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$NIXPKGS_COMMIT.tar.gz
+export NIX_PATH="nixpkgs=$PWD/nix"
 
-STACK2NIX=$(nix-build -A stack2nix --no-out-link)/bin/stack2nix
+STACK2NIX="$(nix-build -A stack2nix --no-out-link)/bin/stack2nix"
 
-COMMIT=d96119645c12905858468a98429f6a887330e7d9
-URL=https://github.com/haskell/haskell-ide-engine.git
+URL="https://github.com/haskell/haskell-ide-engine.git"
+COMMIT="$(jq -r '."haskell-ide-engine".rev' nix/sources.json)"
 
-$STACK2NIX --revision $COMMIT $URL --stack-yaml=stack-8.6.4.yaml > ghc-8.6.nix
-$STACK2NIX --revision $COMMIT $URL --stack-yaml=stack-8.4.4.yaml > ghc-8.4.nix
-$STACK2NIX --revision $COMMIT $URL --stack-yaml=stack-8.2.2.yaml > ghc-8.2.nix
+"$STACK2NIX" --revision "$COMMIT" "$URL" --stack-yaml=stack-8.6.4.yaml > ghc-8.6.nix
+"$STACK2NIX" --revision "$COMMIT" "$URL" --stack-yaml=stack-8.4.4.yaml > ghc-8.4.nix
+"$STACK2NIX" --revision "$COMMIT" "$URL" --stack-yaml=stack-8.2.2.yaml > ghc-8.2.nix
