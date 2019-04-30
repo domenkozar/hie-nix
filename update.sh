73872b184eeb3171f1da4decf87a5a63ea5348d0
R D'Netto <rdnetto@gmail.com>
2019-04-30 05:09:59 -0400

    Set LOCALE_ARCHIVE_2_27 to fix problems running script manually

--- update.sh
+++ update.sh
@@ -7,6 +7,11 @@ export NIX_PATH="nixpkgs=$PWD/nix"
 
 STACK2NIX="$(nix-build -A stack2nix --no-out-link)/bin/stack2nix"
 
+# If we don't set this, the some packages fail to build with Unicode errors
+glibcLocales=$(nix-build --no-out-link -A glibcLocales "<nixpkgs>")
+export LOCALE_ARCHIVE_2_27="${glibcLocales}/lib/locale/locale-archive"
+
+
 URL="https://github.com/haskell/haskell-ide-engine.git"
 COMMIT="$(jq -r '."haskell-ide-engine".rev' nix/sources.json)"
 
